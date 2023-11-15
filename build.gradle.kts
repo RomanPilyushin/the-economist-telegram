739152d4c9b6762d71e55f7b4c97c675f56c5b7b
Roman <rpilyushin@gmail.com>
2023-11-15 18:11:51 +0200

    Change Telegram's API to TelegramBots

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.16.2")
-    implementation ("com.github.pengrad:java-telegram-bot-api:6.9.1")
+    //implementation ("com.github.pengrad:java-telegram-bot-api:6.9.1")
+    implementation ("org.telegram:telegrambots:6.8.0")
 
 }
 
--- src/main/java/org/example/EconomistBot.java
+++ src/main/java/org/example/EconomistBot.java
@@ -1,72 +1,72 @@
 package org.example;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.ParseMode;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.time.Duration;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
 
-public class EconomistBot {
+public class EconomistBot extends TelegramLongPollingBot {
     private static final Logger LOGGER = Logger.getLogger(EconomistBot.class.getName());
     private static final Set<Long> subscribedUsers = new HashSet<>();
-    private static final ZoneId KYIV_ZONE_ID = ZoneId.of("Europe/Kiev");
     private static final String BOT_TOKEN = "6530358402:AAG28dxK3SQCjlPIYayi-aysMNAI8oOZcyw";
+    private static final String BOT_USERNAME = "https://t.me/Theworldinbrief_bot";
 
     public static void main(String[] args) {
-        TelegramBot bot = new TelegramBot(BOT_TOKEN);
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new EconomistBot());
+        } catch (TelegramApiException e) {
+            LOGGER.severe("Error registering bot: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
-        bot.setUpdatesListener(updates -> {
-            for (Update update : updates) {
-                LOGGER.info("Received update: " + update);
-                if (update.message() != null && "/start".equals(update.message().text())) {
-                    long chatId = update.message().chat().id();
-                    subscribedUsers.add(chatId);
-                    LOGGER.info("User subscribed. Chat ID: " + chatId);
-                    sendNewsUpdate(bot, chatId);
-                }
+    @Override
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            long chatId = update.getMessage().getChatId();
+            String inputText = update.getMessage().getText();
+
+            LOGGER.info("Received message: " + inputText + " from Chat ID: " + chatId);
+
+            if ("/start".equals(inputText)) {
+                subscribedUsers.add(chatId);
+                LOGGER.info("User subscribed. Chat ID: " + chatId);
+                sendNewsUpdate(chatId);
             }
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
-        scheduleDailyTask(bot);
-    }
-
-    private static void scheduleDailyTask(TelegramBot bot) {
-        ZonedDateTime now = ZonedDateTime.now(KYIV_ZONE_ID);
-        ZonedDateTime nextRun = now.withHour(9).withMinute(0).withSecond(0);
-        if (now.compareTo(nextRun) > 0) {
-            nextRun = nextRun.plusDays(1);
-        }
-
-        long delay = Duration.between(now, nextRun).getSeconds();
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(() -> sendNewsUpdateToAllSubscribers(bot),
-                delay, TimeUnit.DAYS.toSeconds(1), TimeUnit.SECONDS);
-    }
-
-    private static void sendNewsUpdateToAllSubscribers(TelegramBot bot) {
-        for (long chatId : subscribedUsers) {
-            sendNewsUpdate(bot, chatId);
         }
     }
 
-    private static void sendNewsUpdate(TelegramBot bot, long chatId) {
+    private void sendNewsUpdate(long chatId) {
         try {
             LOGGER.info("Sending news update to Chat ID: " + chatId);
             String formattedContent = WebContentDownloader.downloadContent();
-            bot.execute(new SendMessage(chatId, formattedContent).parseMode(ParseMode.HTML));
-        } catch (Exception e) {
+            execute(SendMessage.builder()
+                    .chatId(String.valueOf(chatId))
+                    .text(formattedContent)
+                    .parseMode("HTML")  // Set parse mode to HTML
+                    .build());
+        } catch (TelegramApiException | IOException e) {
             LOGGER.severe("Error sending news update: " + e.getMessage());
+            e.printStackTrace();
         }
     }
+
 }
